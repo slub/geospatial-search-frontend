65ed5643fd1b9ae9d07b27fd5a9ffef8261ffcea
Thomas Jung <thomas@jung.digital>
2022-05-11 12:28:36 +0200

    UI Improvements for list view
    - Proper UI with dedicated icons for DLF basket
    - a bit more space around elements
    - Optimize visual hierarchy between title, buttons and icons

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -11,6 +11,8 @@ import logo from '../../_image/coverBG.png';
 import {ReactComponent as PolylineIcon} from './line.svg';
 import {ReactComponent as PointIcon} from './point.svg';
 import {ReactComponent as PolygonIcon} from './polygon.svg';
+import {ReactComponent as DlfBasketAdd} from './DlfBasketAdd.svg';
+import {ReactComponent as DlfBasketRemove} from './DlfBasketRemove.svg';
 import {LockOpenOutline} from 'react-ionicons';
 import {LockClosed} from 'react-ionicons';
 import {useCookies} from 'react-cookie';
@@ -23,14 +25,14 @@ import LangLabels from '../../views/MapView/components/Labels';
  * Function component for rendering a ListItem.
  * @returns {*}
  */
-export default function ListItem({ geometryType, id, properties, onClick, onMouseEnter, onMouseLeave, withoutHref = false, showThumbnails = true }) {
+export default function ListItem({geometryType, id, properties, onClick, onMouseEnter, onMouseLeave, withoutHref = false, showThumbnails = true }) {
 
   const titleMaxLength = showThumbnails ? 100 : 200;
   const [cookies, setCookie] = useCookies(['dlf-requests']);
 
   function getBasketIdsFromCookie() {
     const basketCookie = cookies['dlf-requests'];
-    return typeof basketCookie === 'undefined' ?  [] : basketCookie;
+    return typeof basketCookie === 'undefined' ? [] : basketCookie;
   }
 
   function updateCookie() {
@@ -42,69 +44,72 @@ export default function ListItem({ geometryType, id, properties, onClick, onMous
   }
 
   return (
-    <React.Fragment>
-      {properties.restrictions === 'ja' &&
-        <div className="basket-link">
-          {getBasketIdsFromCookie().includes(properties.record_id)
-            ? <div className='digas-basket remove' onClick={() => updateCookie()}>{LangLabels['basket.remove']}</div>
-            : <div className='digas-basket add' onClick={() => updateCookie()}>{LangLabels['basket.add']}</div>
-          }
-        </div>
-      }
-      <a target="_top" className="list-element"
-        href={!withoutHref ? properties.purl : undefined}
-        onClick={() => onClick(id, properties)}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-        rel="noopener noreferrer"
-        title={properties.title}
-      >
-        <div>
-          {showThumbnails === true &&
-            <div className="thumbnail-container">
-              <LazyLoadImage
-                alt={"Vorschaubild von " + properties.record_id}
-                src={properties.thumbnail}
-                onError={(e) => { e.target.onerror = null; e.target.src = logo; e.target.alt = "Kein Vorschaubild vorhanden."; } } />
-            </div>}
-          <div className="content-container">
-            <div className="title">{properties.title.substring(0, titleMaxLength)}{properties.title.length > titleMaxLength && ' [...]'}
-            </div>
-            <div className="footer">
-              <div className="public-docs">
-                {properties.restrictions === 'nein'
-                  ? <LockOpenOutline
-                    color={'#464646'}
-                    title={LangLabels['document.restrictions.no']}
-                    height="20px"
-                    width="20px"/>
-                  : <><LockClosed
-                    color={'#464646'}
-                    title={LangLabels['document.restrictions.yes']}
-                    height="20px"
-                    width="20px"/>
-                  </>}
-              </div>
-              <div className="text">
-                {LangLabels['geosearch.collection']}: {properties.collection} <br/>
-                {LangLabels['geosearch.structure']}: {properties.type === 'monograph' && LangLabels['geosearch.structure.monograph']}
-                {properties.type === 'map' && LangLabels['geosearch.structure.map']}
-                {properties.type === 'volume' && LangLabels['geosearch.structure.volume']} |
-                <span className="tx-dlf-metadata-record_id" data-id={properties.record_id}>{properties.record_id}</span>
-                <span className="tx-dlf-metadata-restrictions" style={{display: 'none'}}>{properties.restrictions}</span>
-              </div>
-              {geometryType !== undefined && (
-                <div className="image">
-                  {geometryType === 'Polygon' && (<PolygonIcon/>)}
-                  {geometryType === 'LineString' && (<PolylineIcon/>)}
-                  {geometryType === 'Point' && (<PointIcon/>)}
-                </div>
-              )}
-            </div>
+    <div className="list-item" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+      {showThumbnails === true &&
+        <div className="thumbnail-container">
+          <LazyLoadImage
+            alt={"Vorschaubild von " + properties.record_id}
+            src={properties.thumbnail}
+            onError={(e) => {
+              e.target.onerror = null;
+              e.target.src = logo;
+              e.target.alt = "Kein Vorschaubild vorhanden.";
+            }}/>
+        </div>}
+      <div className="content-container">
+        <a target="_top" className="title"
+          href={!withoutHref ? properties.purl : undefined}
+          onClick={() => onClick(id, properties)}
+          rel="noopener noreferrer"
+          title={properties.title}
+        >
+          {properties.title.substring(0, titleMaxLength)}{properties.title.length > titleMaxLength && ' [...]'}
+        </a>
+        <div className="footer">
+          <div className="public-docs">
+            {properties.restrictions === 'nein'
+              ? <LockOpenOutline
+                color={'#464646'}
+                title={LangLabels['document.restrictions.no']}
+                height="20px"
+                width="20px"/>
+              : <><LockClosed
+                color={'#464646'}
+                title={LangLabels['document.restrictions.yes']}
+                height="20px"
+                width="20px"/>
+              </>}
           </div>
+          <div className="text">
+            {LangLabels['geosearch.collection']}: {properties.collection} <br/>
+            {LangLabels['geosearch.structure']}: {properties.type === 'monograph' && LangLabels['geosearch.structure.monograph']}
+            {properties.type === 'map' && LangLabels['geosearch.structure.map']}
+            {properties.type === 'volume' && LangLabels['geosearch.structure.volume']} |
+            <span className="tx-dlf-metadata-record_id" data-id={properties.record_id}>{properties.record_id}</span>
+            <span className="tx-dlf-metadata-restrictions" style={{display: 'none'}}>{properties.restrictions}</span>
+          </div>
+          {properties.restrictions === 'ja' &&
+            <div
+              className="basket-link"
+              title={getBasketIdsFromCookie().includes(properties.record_id) ? LangLabels['basket.remove'] : LangLabels['basket.add']}
+              onClick={() => updateCookie()}
+            >
+              {getBasketIdsFromCookie().includes(properties.record_id)
+                ? <DlfBasketRemove/>
+                : <DlfBasketAdd/>
+              }
+            </div>
+          }
+          {geometryType !== undefined && (
+            <div className="type-icon">
+              {geometryType === 'Polygon' && (<PolygonIcon/>)}
+              {geometryType === 'LineString' && (<PolylineIcon/>)}
+              {geometryType === 'Point' && (<PointIcon/>)}
+            </div>
+          )}
         </div>
-      </a>
-    </React.Fragment>
+      </div>
+    </div>
   );
 }
 
